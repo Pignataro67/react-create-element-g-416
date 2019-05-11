e0d492149ef3b47b99f646d50aa391aed336fe9c
Pat  Pignataro <pat6284@msn.com>
2019-05-11 18:49:37 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = React.createElement('h1', {}, 'My First React Code');
+
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+
+const container = React.createElement('div', {}, [title, paragraph]);
+
+ReactDom.render(title, document.getElementById('global'));
